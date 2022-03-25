888d9426fdcc4b9cc35cdf8beed62d34dfedb057
zaida-hammel <99354763+zaida-hammel@users.noreply.github.com>
2022-03-25 16:43:50 +0000

    whitespace

--- main.cs
+++ main.cs
@@ -11,21 +11,16 @@ class Program
     {
         // This function states the area and perimeter of a rectangle
         int length, width, area, perimeter;
-
             Console.WriteLine("This program calculates the area and perimeter of a rectangle.");
             Console.Write("Enter the length (mm): ");
             length = Convert.ToInt32(Console.ReadLine());
             Console.Write("Enter the width (mm): ");
-
             width = Convert.ToInt32(Console.ReadLine());
             area = length * width;
             perimeter = 2 * (length + width);
-
-
             Console.WriteLine("The area is: " + area + "mm².");
             Console.WriteLine("The perimeter is: " + perimeter + "mm.");
             Console.ReadKey();
-
             Console.WriteLine("\nDone.");
     }
 }
